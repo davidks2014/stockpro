690ad7f0ae3a46e521a26befbb42b2037f0513dc
leekoksang <davidks2014@gmail.com>
2023-09-13 19:36:32 +0800

    add optional on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  belongs_to :location, optional: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :location
 end
