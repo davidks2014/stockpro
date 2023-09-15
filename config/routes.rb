9bff076c2fe05756da3150e362bbb80b4948397d
teckguan ng <renraelreve@gmail.com>
2023-09-15 21:48:41 +0800

    amend log in page routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   devise_for :users
-  root to: "pages#home"
+  root to: 'sessions#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
