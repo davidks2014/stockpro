c52b1637bad8c485afcd1923baf4ee198fdaa182
Zunnur Hidayah Zahri <zhidayah@hotmail.com>
2023-09-15 21:06:07 +0800

    removed comments from html new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
