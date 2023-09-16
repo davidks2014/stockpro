6c58716e91fe4507c86344fa186591196ff565b8
leekoksang <davidks2014@gmail.com>
2023-09-16 22:21:59 +0800

    clear

--- app/javascript/controllers/selectlocation_controller.js
+++ app/javascript/controllers/selectlocation_controller.js
@@ -2,14 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="selectlocation"
 export default class extends Controller {
-  static targets = ["items"]
+  static targets = ["form", "items"]
   connect() {
     console.log("Hey, Friend, dont give up !!!selectlocation connected");
   }
 
   select(event) {
-    console.log(event, "location had been selected")
+    //at this line, when choose the location, location had been select
+    // console.log(event, "location had been selected")
+    //at this line, we can check location id by check .value
     console.log(event.currentTarget.value)
-    const url
+    //at this line, we can check form action which the form send information to where
+    console.log(this.formTarget.action)
+    const url = `${this.formTarget.action}?location=${event.currentTarget.value}`
+    //in this line, url = http://localhost:3000/requests/new?location=3
+    console.log(url)
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        //console.log(data)
+        console.log(this.itemsTarget.outerHTML)
+      })
   }
 }
