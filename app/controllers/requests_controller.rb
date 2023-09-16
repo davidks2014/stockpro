bd4197cb54b433982b12ca89b3e7bca9d8356f66
leekoksang <davidks2014@gmail.com>
2023-09-16 23:10:02 +0800

    practice done

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,13 +4,16 @@ class RequestsController < ApplicationController
     @materials = Material.all
     @equipment = Equipment.all
     @locations = Location.all
-    respond_to do |format|
-      format.html # Follow regular flow of Rails
-      format.text { render partial: "requests/table", locals: {materials: @materials, equipment: @equipment}, formats: [:html] }
-    end
+
     if params[:location].present?
       @materials = @materials.where(location_id: params[:location])
       @equipment = @equipment.where(location_id: params[:location])
     end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "requests/table", locals: {materials: @materials, equipment: @equipment}, formats: [:html] }
+    end
+
   end
 end
