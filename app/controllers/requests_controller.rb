c905690cd2d58445a368c718c530f80b32859377
leekoksang <davidks2014@gmail.com>
2023-09-16 22:28:14 +0800

    clear

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,5 +4,9 @@ class RequestsController < ApplicationController
     @materials = Material.all
     @equipment = Equipment.all
     @locations = Location.all
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "requests/table", locals: {materials: @materials, equipment: @equipment}, formats: [:html] }
+    end
   end
 end
