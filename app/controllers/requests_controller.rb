126c34f234464679c9a51166513be99b6d790d67
leekoksang <davidks2014@gmail.com>
2023-09-17 15:58:57 +0800

    clear

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,5 +4,16 @@ class RequestsController < ApplicationController
     @materials = Material.all
     @equipment = Equipment.all
     @locations = Location.all
+
+    if params[:location].present?
+      #they will only find material = (location_id = 2)
+      @materials = Material.where(location_id: params[:location])
+      @equipment = Equipment.where(location_id: params[:location])
+    end
+
+    respond_to do |format|
+      format.html #follow back the rails process
+      format.text { render partial: 'requests/table', locals: { materials: @materials, equipment: @equipment} }
+    end
   end
 end
---
+++ app/javascript/controllers/selectlocation_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="selectlocation"
+export default class extends Controller {
+  static targets = ["form", "items"]
+  connect() {
+    console.log("Hello from the selectlocation_controller Stimulus controller!")
+  }
+
+  filter(event) {
+    // console.log(event, "this location had been selected")
+    console.log(event.currentTarget.value)
+    // console.log("this should be form", this.formTarget)
+    // console.log("this should be items", this.itemsTarget)
+    // console.log("where this form sending information", this.formTarget.action)
+
+    const url = `${this.formTarget.action}?location=${event.currentTarget.value}`
+    // console.log(url)
+    const option = {
+      method: "GET",
+      headers: {
+        "Accept": "text/plain" ,
+      },
+    }
+    fetch(url, option)
+      .then(response => response.text())
+      .then(data => {
+        this.itemsTarget.outerHTML = data
+      })
+  }
+}
