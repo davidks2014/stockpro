8a4fa1863219a441ebc33cf9ac51e21d3a25ada6
Ping Yu Choi <choipy7@gmail.com>
2023-09-18 15:54:06 +0800

    Created auto-zoom to viewable markers in map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,12 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static values = { apiKey: String }
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: this.element,
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
     });
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat])
+    })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 100 })
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(this.map);
+    })
   }
 }
