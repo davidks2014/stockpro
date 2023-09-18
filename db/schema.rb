9906c9551b0b5e6195c70274037aa329a21c0c39
teckguan ng <renraelreve@gmail.com>
2023-09-18 22:51:38 +0800

    stash

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_09_16_161627) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_09_18_024709) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
