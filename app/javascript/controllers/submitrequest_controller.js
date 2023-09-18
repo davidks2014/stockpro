fb553036aa8c60162b9def4393df635285717812
leekoksang <davidks2014@gmail.com>
2023-09-18 20:39:31 +0800

    clear

--- app/javascript/controllers/submitrequest_controller.js
+++ app/javascript/controllers/submitrequest_controller.js
@@ -11,9 +11,34 @@ export default class extends Controller {
     event.preventDefault()
     const form = event.currentTarget
 
+    const location_id = this.locationTarget.value
+
+    const inputs = document.querySelectorAll('input[name="qty"]')
+
+    console.log(inputs)
+
+    const request = {
+      location_id: location_id,
+      items: []
+    }
+
+    inputs.forEach((input)=>{
+      if (input.value) {
+        request.items.push({
+          item_type: input.id.split("-")[0],
+          item_id: input.id.split("-")[1],
+          qty: input.value
+        })
+      }
+    })
+
+    const formData = new FormData(form).append("requestBody", JSON.stringify(request))
+
     fetch(form.action, {
       method: "POST",
-      body: new FormData(form)
+      body: formData
     })
   }
 }
+// Request.new (location_id)
+// ItemRequest.new (item_type, item_id, qty, request_id)
