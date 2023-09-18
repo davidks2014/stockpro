30bff76e77cad650e413744c8f39802d87e59958
Ping Yu Choi <choipy7@gmail.com>
2023-09-18 20:42:04 +0800

    Added custom markers(robot-logo), info-window to map

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -8,9 +8,11 @@ class SitesController < ApplicationController
     @markers = @sites.geocoded.map do |site|
       {
         lat: site.latitude,
-        lng: site.longitude
+        lng: site.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { site: site }),
+        marker_html: render_to_string(partial: "marker", locals: {site: site}) # Pass the flat to the partial
       }
-  end
+    end
   end
 
   # GET /sites/1
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -27,9 +27,23 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
-      .setLngLat([marker.lng, marker.lat])
-      .addTo(this.map);
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+
+      // new mapboxgl.Marker()
+      // .setLngLat([marker.lng, marker.lat])
+      // .setPopup(popup)
+      // .addTo(this.map);
+
+      // Create a HTML element for your custom marker
+      const customMarker = document.createElement("div")
+      customMarker.innerHTML = marker.marker_html
+
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map)
+
     })
   }
 }
