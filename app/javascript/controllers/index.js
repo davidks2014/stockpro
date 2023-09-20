5fefbf7630d9962beeb42c2298930fd94f9f729a
leekoksang <davidks2014@gmail.com>
2023-09-20 21:03:36 +0800

    clear

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,6 @@
 
 import { application } from "./application"
 
-import ChangetableController from "./changetable_controller"
-application.register("changetable", ChangetableController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
