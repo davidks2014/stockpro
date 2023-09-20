9e59a458fff97b32585103b6df3658a2d281a3ba
Zunnur Hidayah Zahri <zhidayah@hotmail.com>
2023-09-20 15:55:47 +0800

    merging from master

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_09_16_161627) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_09_18_024709) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,13 +95,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_024709) do
 
   create_table "requests", force: :cascade do |t|
     t.bigint "location_id", null: false
-    t.bigint "user_id", null: false
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "original_location_id"
     t.index ["location_id"], name: "index_requests_on_location_id"
-    t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
   create_table "sites", force: :cascade do |t|
@@ -140,6 +134,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_024709) do
   add_foreign_key "material_stockcounts", "locations"
   add_foreign_key "materials", "locations"
   add_foreign_key "requests", "locations"
-  add_foreign_key "requests", "users"
   add_foreign_key "users", "locations"
 end
