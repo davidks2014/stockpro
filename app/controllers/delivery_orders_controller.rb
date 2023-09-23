058e276f78a89eaa7b9a09cb4662f230b191d553
leekoksang <davidks2014@gmail.com>
2023-09-23 14:29:00 +0800

    qty updated once delivered received

--- app/controllers/delivery_orders_controller.rb
+++ app/controllers/delivery_orders_controller.rb
@@ -35,6 +35,21 @@ class DeliveryOrdersController < ApplicationController
     @delivery_order.update(delivery_status: 'Received')
 
     redirect_to delivery_orders_path, notice: "Delivery order received"
+
+    #now we need to update the qty in location
+
+    @delivery_order.item_requests.each do |item_request|
+      item_request_qty = item_request.qty
+      existing_qty = item_request.item.qty
+      sent_material_location = item_request.item.location
+      received_material_location = Location.find(item_request.request.original_location_id)
+
+
+      updated_project_material = Material.where(location: received_material_location, name: item_request.item.name).first
+
+      updated_warehouse = item_request.item.update(qty: existing_qty - item_request_qty)
+      updated_project_material.update(qty: updated_project_material.qty + item_request_qty)
+    end
   end
 
 end
