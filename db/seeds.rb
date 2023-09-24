7fe4deb056f3d7f737c999ba72ba9c439bd70f4c
teckguan ng <renraelreve@gmail.com>
2023-09-24 08:08:27 +0800

    seeded less materials and equipment

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,13 @@ locations.each do |attributes|
   location = Location.create!(attributes)
   puts "Created #{location.name}"
   rose = Material.create({category: "flower", name: "rose", cost_code: "MFR", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "no.", location: location})
-  hibiscus = Material.create({category: "flower", name: "hibiscus", cost_code: "MFH", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "no.", location: location})
-  lily = Material.create({category: "flower", name: "lily", cost_code: "MFL", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "no.", location: location})
+
   brocoli = Material.create({category: "vegetable", name: "brocoli", cost_code: "MVB", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "kg", location: location})
-  spinach = Material.create({category: "vegetable", name: "spinach", cost_code: "MVS", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "kg", location: location})
-  cabbage = Material.create({category: "vegetable", name: "cabbage", cost_code: "MVC", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "kg", location: location})
+
   orange = Material.create({category: "fruit", name: "orange", cost_code: "MFO", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "kg", location: location})
-  apple = Material.create({category: "fruit", name: "apple", cost_code: "MFA", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "kg", location: location})
-  mango = Material.create({category: "fruit", name: "mango", cost_code: "MFM", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "kg", location: location})
+
   oak = Material.create({category: "tree", name: "oak", cost_code: "MTO", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "no.", location: location})
-  maple = Material.create({category: "tree", name: "maple", cost_code: "MTM", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "no.", location: location})
-  banyan = Material.create({category: "tree", name: "banyan", cost_code: "MTB", unit_price: rand(1.00..10.00).round(2), qty: rand(0.00..10.00).round(0), uom: "no.", location: location})
+
   puts " #{location.name} Creating equipment..."
 
   location.materials.each  do |material|
@@ -31,17 +27,13 @@ locations.each do |attributes|
   end
 
   vacuum = Equipment.create({category: "electronics", name: "vacuum", qty: rand(0..10), location: location})
-  table = Equipment.create({category: "furniture", name: "table", qty: rand(0..10), location: location})
-  chair = Equipment.create({category: "furniture", name: "chair", qty: rand(0..10), location: location})
+
   shelf = Equipment.create({category: "furniture", name: "shelf", qty: rand(0..10), location: location})
-  bicycle = Equipment.create({category: "sports", name: "bicycle", qty: rand(0..10), location: location})
-  basketball = Equipment.create({category: "sports", name: "basketball", qty: rand(0..10), location: location})
+
   goggles = Equipment.create({category: "sports", name: "goggles", qty: rand(0..10), location: location})
+  
   piano = Equipment.create({category: "instrument", name: "piano", qty: rand(0..10), location: location})
-  violin = Equipment.create({category: "instrument", name: "violin", qty: rand(0..10), location: location})
-  flute = Equipment.create({category: "instrument", name: "flute", qty: rand(0..10), location: location})
-  fridge = Equipment.create({category: "electronics", name: "fridge", qty: rand(0..10), location: location})
-  toaster = Equipment.create({category: "electronics", name: "toaster", qty: rand(0..10), location: location})
+
 
   location.equipment.each  do |equipment|
     puts "#{equipment.name} created"
