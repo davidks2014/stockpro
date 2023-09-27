db545cfc2c27f95248156474af275a76de5c6427
Zunnur Hidayah Zahri <zhidayah@hotmail.com>
2023-09-27 19:41:37 +0800

    clearing commits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_045946) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_162617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,6 +90,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_045946) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "material_movements", force: :cascade do |t|
+    t.float "unit_rate"
+    t.float "qty"
+    t.datetime "update_date"
+    t.bigint "location_id", null: false
+    t.bigint "material_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "remarks"
+    t.float "amount"
+    t.index ["location_id"], name: "index_material_movements_on_location_id"
+    t.index ["material_id"], name: "index_material_movements_on_material_id"
+  end
+
   create_table "material_stockcounts", force: :cascade do |t|
     t.string "category"
     t.string "name"
@@ -113,6 +127,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_045946) do
     t.bigint "location_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "amount"
+    t.datetime "update_date"
     t.index ["location_id"], name: "index_materials_on_location_id"
   end
 
@@ -134,6 +150,33 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_045946) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+  end
+
+  create_table "stockcounts", force: :cascade do |t|
+    t.datetime "update_date"
+    t.float "qty"
+    t.bigint "location_id", null: false
+    t.bigint "material_id", null: false
+    t.float "unit_rate"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "diff"
+    t.index ["location_id"], name: "index_stockcounts_on_location_id"
+    t.index ["material_id"], name: "index_stockcounts_on_material_id"
+  end
+
+  create_table "update_materials", force: :cascade do |t|
+    t.float "unit_rate"
+    t.datetime "update_date"
+    t.bigint "location_id", null: false
+    t.bigint "material_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "qty"
+    t.float "amount"
+    t.index ["location_id"], name: "index_update_materials_on_location_id"
+    t.index ["material_id"], name: "index_update_materials_on_material_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -159,9 +202,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_045946) do
   add_foreign_key "item_deliveries", "item_requests"
   add_foreign_key "item_requests", "delivery_orders"
   add_foreign_key "item_requests", "requests"
+  add_foreign_key "material_movements", "locations"
+  add_foreign_key "material_movements", "materials"
   add_foreign_key "material_stockcounts", "locations"
   add_foreign_key "materials", "locations"
   add_foreign_key "requests", "locations"
   add_foreign_key "requests", "users"
+  add_foreign_key "stockcounts", "locations"
+  add_foreign_key "stockcounts", "materials"
+  add_foreign_key "update_materials", "locations"
+  add_foreign_key "update_materials", "materials"
   add_foreign_key "users", "locations"
 end
