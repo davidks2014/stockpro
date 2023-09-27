0aeef0ab9c6dddc44b7a69b597340dd0c960f1bb
Ping Yu Choi <choipy7@gmail.com>
2023-09-27 12:24:38 +0800

    Added user type (engr, mgr) beside avatar in navbar

--- db/seeds-backup.rb
+++ db/seeds-backup.rb
@@ -81,7 +81,7 @@ end
   puts "#{pingyu.name} created"
 
 puts " Creating user manager..."
-company = Location.create({name: "HQ", address: "Singapore"})
+company = Location.create({name: "company", address: "Singapore"})
 manager = User.create({name: "Manager", role: "manager", email: "manager@gmail.com", password: "123456"})
 puts "#{manager.name} created"
 
