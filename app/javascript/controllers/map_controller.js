63dac5e0c8904eedfe11c1c2923bf85e3aca962f
Ping Yu Choi <choipy7@gmail.com>
2023-09-29 19:50:05 +0800

    linked locations on map to location show page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -28,7 +28,7 @@ export default class extends Controller {
     this.markersValue.forEach((marker) => {
       bounds.extend([marker.lng, marker.lat])
     })
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 100 })
+    this.map.fitBounds(bounds, { padding: 80, maxZoom: 15, duration: 100 })
   }
 
   #addMarkersToMap() {
