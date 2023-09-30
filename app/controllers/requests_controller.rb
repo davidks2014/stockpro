32abebcca306af8a81f492dbc701874fc6ac8955
leekoksang <davidks2014@gmail.com>
2023-09-30 16:43:34 +0800

    conflict fix

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -45,7 +45,7 @@ class RequestsController < ApplicationController
           item_id: item["item_id"],
           qty: item["qty"],
           request: @request,
-          del_approv_status: "Pending engineer approval"
+          del_approv_status: "Pending"
         )
 
         item_requests << item_request
