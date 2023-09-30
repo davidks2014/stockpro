9f6ab266f11433f08f0e5ca4a935b9a3c6a91175
Ping Yu Choi <choipy7@gmail.com>
2023-09-30 10:09:20 +0800

    Resolved popup msg window and links to location show page via icon/location name

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -33,7 +33,7 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      // const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
 
       // Create a HTML element for your custom marker
       const customMarker = document.createElement("div")
@@ -42,7 +42,7 @@ export default class extends Controller {
       // Pass the element as an argument to the new marker
       new mapboxgl.Marker(customMarker)
         .setLngLat([marker.lng, marker.lat])
-        .setPopup(popup)
+        // .setPopup(popup)
         .addTo(this.map)
 
     })
