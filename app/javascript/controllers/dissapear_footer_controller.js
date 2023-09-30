3749d37638005041fbbee022322d33042f3215df
leekoksang <davidks2014@gmail.com>
2023-09-30 15:28:35 +0800

    footer animation

--- app/javascript/controllers/dissapear_footer_controller.js
+++ app/javascript/controllers/dissapear_footer_controller.js
@@ -2,17 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dissapear-footer"
 export default class extends Controller {
+  static targets = ["footer"]
   connect() {
-    console.log("hihi")
-    window.addEventListener("scroll",()=>{
-      const scrollY = window.scrollY
-      const scrollThreshold = 100;
+    console.log(this.footerTarget);
 
-      if (scrollY >= scrollThreshold) {
-        this.element.style.opacity = 0
-      } else {
-        this.element.style.opacity = 1
-      }
-    })
+  }
+
+  hide() {
+    const scrollY = window.scrollY
+    // const scrollThreshold = 100;
+    console.log(scrollY);
+
+    const opacity = 1 - (scrollY / 100)
+    console.log("opacity: ", opacity);
+    this.footerTarget.style.opacity = opacity
+
+    if (opacity < 0) {
+      this.footerTarget.style.display = 'none'
+    } else {
+      this.footerTarget.style.display = 'block'
+    }
+
+
+
+    // if (scrollY >= scrollThreshold) {
+    //   this.footerTarget.style.opacity = 0
+    //   setTimeout(() => {
+    //     this.footerTarget.classList.add("d-none")
+    //   }, 1000)
+    // } else {
+    //   this.footerTarget.classList.remove("d-none")
+    //   setTimeout(() => {
+    //     this.footerTarget.style.opacity = 1
+    //   }, 1000)
+    // }
   }
 }
